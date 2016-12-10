f7800a0d60ac0c9d5cffa6bd07db2a79431585b3
bsaxen <benny.saxen@gmail.com>
2016-12-10 12:21:50 +0100

    Update xsim_ctrl.php

--- xsim/xsim_ctrl.php
+++ xsim/xsim_ctrl.php
@@ -1,88 +1,90 @@
-<html>
-<head>
-<script>
-function showHint(str) {
-    if (str.length == 0) { 
-        document.getElementById("txtHint").innerHTML = "";
-        return;
-    } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET", "xsim_ajax.php?id=" + str, true);
-        xmlhttp.send();
-    }
-}
-window.setTimeout(showHint("benny"),1000);
-</script>
-</head>
-<body>
+  <html>
+  <head>
+  </head>
+  <?php
+  echo("<body>");
+  echo("<h1>XSIM Remote Control</h1>");
 
-
-<p>Suggestions: <span id="txtHint"></span></p>
-
-<?php
-
-function saveOrder($id,$order)
-{
-  $file = $id.'.order';
-  $fh = fopen($file, 'w') or die("saveOrder can't open file: $file");
-  fwrite($fh, $order);
-  fclose($fh);
-}
-function listAllClients()
-{
-  system("ls *.poll > pollList.work"); 
-  $file = 'pollList.work';
-  $fh = fopen($file, 'r') or die("no poll list");
-  while(!feof($fh)) 
+  if(isset($_GET['client']))
   {
-     $row = fgets($fh);
-     $row = trim($row);
-     $len = strlen($row);
-     if($len >0)
-     {
-      $temp = explode(".",$row);
-      $id = $temp[0];
-      echo("<a href=\"xsim_ctrl.php?client=$id\">$id</a>");
-      $fh2 = fopen($row, 'r') or die("   Error! unable to open file: ($row) =$len");
-      $row = fgets($fh2);
-      fclose($fh2);
-      echo(" [$row]<br>");
-     }
+    $client = $_GET['client'];
+    echo("
+    <form action=\"xsim_ctrl.php\" method=\"post\">
+          <input type=\"hidden\" name=\"client\" value=\"$client\" />
+          <input type=\"text\" name=\"order\" />
+          <button type=\"submit\">Execute $client</button>
+    </form>
+  ");
+    $file = $client.'.res';
+    if(file_exists($file))
+    {
+      echo("<br>=====================<br>");
+      $fh = fopen($file, 'r');
+      while(!feof($fh)) 
+      {
+        $row = fgets($fh);
+        $row = trim($row);
+        $len = strlen($row);
+        if($len >0)
+        {
+          echo $row;echo('<br>');
+        }
+      }
+      fclose($fh);
+      echo("=====================<br>");
+    }
   }
-  fclose($fh);
-}
-
-if ($_SERVER['REQUEST_METHOD'] == "POST")
-{
-    $client = $_POST['client'];
-    $order = $_POST['order'];
-    saveOrder($client,$order);
-}
 
 
-echo("<h1>XSIM Remote Control</h1>");
 
-if(isset($_GET['client']))
-{
-  $client = $_GET['client'];
-  echo("
-  <form action=\"xsim_ctrl.php\" method=\"post\">
-        <input type=\"hidden\" name=\"client\" value=\"$client\" />
-        <input type=\"text\" name=\"order\" />
-        <button type=\"submit\">Execute $client</button>
-  </form>
-");
-}
+  function saveOrder($id,$order)
+  {
+    $file = $id.'.order';
+    $fh = fopen($file, 'w');
+    fwrite($fh, $order);
+    fclose($fh);
+  }
+  function listAllClients()
+  {
+    system("ls *.poll > pollList.work"); 
+    $file = 'pollList.work';
+    if(file_exists($file))
+    {
+      $fh = fopen($file, 'r');
+      while(!feof($fh)) 
+      {
+        $row = fgets($fh);
+        $row = trim($row);
+        $len = strlen($row);
+        if($len >0)
+        {
+          $temp = explode(".",$row);
+          $id = $temp[0];
+          echo("<a href=\"xsim_ctrl.php?client=$id\">$id</a>");
+          if(file_exists($row))
+          { 
+            $fh2 = fopen($row, 'r');
+            $row = fgets($fh2);
+            fclose($fh2);
+            echo(" [$row]<br>");
+          }
+        }
+      }
+    fclose($fh);
+    }
+  }
 
-// Create links får all polling clients
+  if ($_SERVER['REQUEST_METHOD'] == "POST")
+  {
+      $client = $_POST['client'];
+      $order = $_POST['order'];
+      saveOrder($client,$order);
+  }
 
 
-listAllClients();
-?>
-</body>
-</html>
+  // Create links får all polling clients
+
+  listAllClients();
+  ?>
+  </body>
+  </html>

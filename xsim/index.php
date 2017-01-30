43b5500751738ec192de42f2d31ba00598d1338f
bsaxen <benny.saxen@gmail.com>
2017-01-30 13:48:21 +0100

    Update index.php

--- xsim/index.php
+++ xsim/index.php
@@ -1,7 +1,23 @@
 <?php
+echo("<!doctype html>");
 
-echo("RPi list");
-echo("Running processes");
-echo("Command");
+echo("<html>");
+echo("<head>");
+    echo("<title>XSIM Remote Control</title>");
+    echo("<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />");
+    echo("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js\"></script>");
+    echo("<style>");
+    echo("body {
+        text-align: center;
+    }");
+    echo("</style>");
+echo("</head>");
+echo("<body>");
+echo("<div id=\"container\">");
+echo("<a href=index.php?action=clear> Clear </a>");
+echo("</div>");
+
+echo("</body>");
+echo("</html>");
 
 ?>

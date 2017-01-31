e621f53affb1bfb488a1b78184d57ecbfa862e47
bsaxen <benny.saxen@gmail.com>
2017-01-31 15:36:43 +0100

    css improvements

--- xsim/index.php
+++ xsim/index.php
@@ -23,8 +23,27 @@ echo("<head>");
     echo("
     body {
         text-align: center;
+        margin-bottom: 100px;
+    }
+    #d_container{
+        position: relative;
+        background: purple;
+        border: 1px solid #AAAAAA;
+        padding: 10px;
+        color: #fff0;
+        width: 100%;
+    }
+    #d_center{
+        position: relative;
+        background: grey;
+        border: 1px solid #AAAAAA;
+        padding: 10px;
+        color: #fff0;
+        width: 100%;
     }
     #d_left{
+        display: inline-block;
+        float: left;
         background: green;
         border: 1px solid #AAAAAA;
         padding: 10px;
@@ -32,13 +51,16 @@ echo("<head>");
         width: 30%;
     }
     #d_middle{
+        display: inline-block;
         background: yellow;
         border: 1px solid #AAAAAA;
         padding: 10px;
         color: #ff0f;
         width: 30%;
     }
-    #d_right{
+    #d_right{i
+        display: inline-block;
+        float: right;
         background: red;
         border: 1px solid #AAAAAA;
         padding: 10px;
@@ -46,7 +68,8 @@ echo("<head>");
         width: 30%;
     }
     #d_header{
-        background: black;
+        position:relative;
+        background: cornsilk;
         border: 1px solid #AAAAAA;
         padding: 10px;
         color: #0000;
@@ -54,6 +77,9 @@ echo("<head>");
         width: 100%;
     }
     #d_footer{
+        position:fixed;
+        bottom: 0;
+        left: 0;
         background: white;
         border: 1px solid #AAAAAA;
         padding: 10px;
@@ -65,23 +91,29 @@ echo("<head>");
 echo("</style>");
 echo("</head>");
 echo("<body>");
-echo("<div id=\"container\">");
-echo("<a href=index.php?action=clear> Clear </a>");
-echo("<div id=\"d_header\">");
+echo("<div id=\"d_container\">");
+  echo("<a href=index.php?action=clear> Clear </a>");
+  echo("<div id=\"d_header\">");
     echo("This is header");
-echo("</div>");
-echo("<div id=\"d_left\">");
-    //echo(lib_display("UNIT_LIST"));
+  echo("</div>");
+  echo("<div id=\"d_center\">");
+  echo("center");
+  echo("<div id=\"d_left\">");
+   echo("left");
     echo("<span id=\"list\"></span>");
-echo("</div>");
-echo("<div id=\"d_middle\">");
+  echo("</div>");
+  echo("<div id=\"d_middle\">");
+    echo("middle");
     echo("<span id=\"response\"></span>");
-echo("</div>");
-echo("<div id=\"d_right\">");
+  echo("</div>");
+  echo("<div id=\"d_right\">");
+    echo("right");
     echo("<span id=\"history\"></span>");
-echo("</div>");
-echo("<div id=\"d_footer\">");
+  echo("</div>");
+  echo("</div>");
+  echo("<div id=\"d_footer\">");
     echo("This is footer");
+  echo("</div>");
 echo("</div>");
 /*echo("
 <script type=\"text/javascript\">
